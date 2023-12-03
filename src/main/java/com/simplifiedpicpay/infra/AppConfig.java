d888447dd0157d8ddf20e539e4a781293c5055a2
odivanmarco <odivan.marcos@ufvjm.edu.br>
2023-12-03 16:59:40 -0300

    feat: Created AppConfig and NotificationDto

---
+++ src/main/java/com/simplifiedpicpay/infra/AppConfig.java
@@ -0,0 +1,11 @@
+package com.simplifiedpicpay.infra;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+public class AppConfig {
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
