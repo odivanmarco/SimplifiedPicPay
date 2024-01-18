14fa4e175bc453f2309e47ff109f1cb8a2e03b4a
odivanmarco <odivan.marcos@ufvjm.edu.br>
2024-01-18 08:59:18 -0300

    feat: Implementations in Users

--- src/main/java/com/simplifiedpicpay/services/Interfaces/IUserService.java
+++ src/main/java/com/simplifiedpicpay/services/Interfaces/IUserService.java
@@ -1,11 +1,17 @@
 package com.simplifiedpicpay.services.Interfaces;
 
 import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 
 import com.simplifiedpicpay.domain.user.User;
+import com.simplifiedpicpay.dtos.UserDto;
 
 public interface IUserService {
     public void validateTransaction(User sender, BigDecimal amount) throws Exception;
     public User findUserById(Long id) throws Exception;
     public void saveUser(User user);
+    public User creatUser(UserDto dto);
+    public List<User> getAll();
 }
